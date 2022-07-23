90b1a6a8b02943515f0498bdbf38421ff4b3472c
fabiobranis <fabiobranis@gmail.com>
2022-07-23 10:50:02 -0300

    Updates error logging

--- src/services/jobs/log-members-errors.ts
+++ src/services/jobs/log-members-errors.ts
@@ -1,8 +1,21 @@
 import { BatchListMembersError } from '../../libs/mail-marketing-client';
 import pino from 'pino';
+import { EOL } from 'os';
 
 const logger = pino();
 
 export function logMembersErrors(errors: Array<BatchListMembersError>) {
-  logger.warn(`Contacts not synced: ${errors}`);
+  logger.warn(
+    'Contacts not synced:' + errors.map((error) => buildErrorMessage(error))
+  );
+}
+
+function buildErrorMessage({
+  emailAddress,
+  error,
+  errorCode,
+  field,
+  fieldMessage
+}: BatchListMembersError): string {
+  return `Email Address: ${emailAddress}, error: ${error}, code: ${errorCode}, field: ${field}, message: ${fieldMessage}${EOL}`;
 }
